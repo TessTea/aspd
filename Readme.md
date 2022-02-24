1b9befa237566a0e8c6081f434ffc9c9f8621338
Artem Borisov <artem.borisov@kindda.app>
2022-02-24 14:25:08 +0100

    adding profile

--- Readme.md
+++ Readme.md
@@ -1,5 +1,9 @@
 
+  
+
 # Amazon Switch Profile Default - simple cli for switchin between aws profiles through the [default] credential section.
+
+  
 
   
 
@@ -7,7 +11,11 @@
 
   
 
+  
+
 An alternative to standart aws asp utility, that switches across multiple profiles rewriting the default one.
+
+  
 
   
 
@@ -15,63 +23,86 @@ An alternative to standart aws asp utility, that switches across multiple profil
 
   
 
+  
+
 ```shell
-
 npm install -g aspd
-
 ```
+
+  
 
   
 
 ## Usage:
 
+  ### Profile switching:
+
   
 
 At the first place your ~/.aws/credentials file should have the structure similar to this:
 
+  
+
 ```ini
 [default]
-aws_access_key_id = **********
-aws_secret_access_key = **********
+aws_access_key_id  = **********
+aws_secret_access_key  = **********
 [profile_1]
-aws_access_key_id = **********
-aws_secret_access_key = **********
+aws_access_key_id  = **********
+aws_secret_access_key  = **********
 [profile_2]
-aws_access_key_id = **********
-aws_secret_access_key = **********
+aws_access_key_id  = **********
+aws_secret_access_key  = **********
 [profile_3]
-aws_access_key_id = **********
-aws_secret_access_key = **********
+aws_access_key_id  = **********
+aws_secret_access_key  = **********
 ```
+
+  
 
 If you have an ~/.aws/config file its struct assums to be like this:
 
+  
+
 ```ini
 [default]
-region = ********
+region  = ********
 [profile_1]
-region = ********
+region  = ********
 [profile_2]
-region = ********
+region  = ********
 [profile_3]
-region = ********
+region  = ********
 ```
 
+  
+
 ```shell
-
 aspd
-
 ```
 
 Then just choose profile that you want to use as default.
+
+### Add new profile:
+
+```shell
+aspd add
+```
+  Then just folow  the flow.
 
   
 
 ## !!! Reminder !!:
 
+  
+  
 
 Your default profile will be rewriten with the chosen one, so don't forget to save it if you need.
 
+  
+
 ## TODO:
 
-- Use AWS_CONFIG_FILE & AWS_SHARED_CREDENTIALS_FILE env variables as an alternative files paths. 
+  
+
+- Use AWS_CONFIG_FILE & AWS_SHARED_CREDENTIALS_FILE env variables as an alternative files paths.
--- index.js
+++ index.js
@@ -6,15 +6,42 @@ import { createSpinner } from "nanospinner";
 import chalk from "chalk";
 import fs from 'fs';
 
+
+const args = process.argv.slice(2);
 const sleep = (ms = 1000) => new Promise((r) => setTimeout(r, ms));
 const credentials = propertiesReader(`${process.env.HOME}/.aws/credentials`);
+
+const regionList = [
+    'af-south-1',
+    'ap-northeast-1',
+    'ap-northeast-3',
+    'ap-southeast-1',
+    'ca-central-1',
+    'eu-north-1',
+    'eu-west-1',
+    'eu-west-3',
+    'sa-east-1',
+    'us-east-2',
+    'us-west-2',                     
+    'ap-east-1',
+    'ap-northeast-2',
+    'ap-south-1',
+    'ap-southeast-2',
+    'eu-central-1',
+    'eu-south-1',
+    'eu-west-2',
+    'me-south-1',
+    'us-east-1',
+    'us-west-1' 
+]
+
 let config;
 let configList = [];
 const isConfigExist = fs.existsSync(`${process.env.HOME}/.aws/config`)
 const unsortedConfigList = [];
 if (isConfigExist) {
     config = propertiesReader(`${process.env.HOME}/.aws/config`);
-    
+
     config.each((key, value) => {
         unsortedConfigList.push(key.split('.')[0])
     });
@@ -22,9 +49,9 @@ if (isConfigExist) {
     configList.forEach(element => {
         if (element === 'default') return configList.splice(element, 1);
     });
-  } else {
+} else {
     console.log("DOES NOT exist:", `${process.env.HOME}/.aws/config`);
-  }
+}
 
 
 const unsortedProfileList = [];
@@ -36,7 +63,7 @@ profileList.forEach(element => {
     if (element === 'default') return profileList.splice(element, 1);
 });
 
-async function askProfile() {
+async function chooseProfile(doRemove) {
     const answer = await inquirer.prompt({
         name: 'profileName',
         type: "list",
@@ -44,28 +71,81 @@ async function askProfile() {
         choices: profileList
     })
 
-    return handleAnswer(answer.profileName);
-}
+    if(doRemove){
 
-async function handleAnswer(profile) {
+    } else{
+        return handleAnswerChoose(answer.profileName)
+    }
+};
+
+async function handleAnswerChoose(profile) {
     const spinner = createSpinner('Switching profile...').start();
     await sleep();
 
-    
+
     const awsAccessKeyId = credentials.get(`${profile}.aws_access_key_id`);
     const awsSecretAccessKey = credentials.get(`${profile}.aws_secret_access_key`);
     credentials.set(`default.aws_access_key_id`, awsAccessKeyId);
     credentials.set(`default.aws_secret_access_key`, awsSecretAccessKey);
-    if (isConfigExist){
+    if (isConfigExist) {
         const awsRegion = config.get(`${profile}.region`);
-        awsRegion ? config.set(`default.region`, awsRegion): config.set(`default.region`, '');
+        awsRegion ? config.set(`default.region`, awsRegion) : config.set(`default.region`, '');
         await config.save(`${process.env.HOME}/.aws/config`);
-        spinner.success({text: `Default region switched to [${chalk.greenBright(awsRegion)}]`});
+        spinner.success({ text: `Default region switched to [${chalk.greenBright(awsRegion)}]` });
     }
     await credentials.save(`${process.env.HOME}/.aws/credentials`);
-    spinner.success({text:`Default profile switched to [${chalk.greenBright(profile)}]`});
-}
+    spinner.success({ text: `Default profile switched to [${chalk.greenBright(profile)}]` });
+};
 
-console.clear();
-askProfile();
+async function addProfile() {
+    const answer = await inquirer.prompt([{
+        name: 'profileName',
+        type: 'input',
+        message: 'Write down name for a new profile: '
+    },
+    {
+        name: 'awsAccessKeyId',
+        type: 'input',
+        message: 'Write down aws_access_key_id for a new profile: '
+    },
+    {
+        name: 'awsSecretAccessKey',
+        type: 'input',
+        message: 'Write down aws_secret_access_key for a new profile: '
+    },
+    {
+        name: 'awsRegion',
+        type: 'list',
+        message: 'Choose your default aws region for this profile: ',
+        choices: regionList
+    }]);
+    // console.log(answer);
+    addAnswerHandle(answer);
+};
 
+// async function deleteAnswerHandle(profile){
+
+// }
+
+async function addAnswerHandle(profile){
+    const spinner = createSpinner('Adding new profile...').start();
+    await sleep();
+
+    credentials.set(`${profile.profileName}.aws_access_key_id`, profile.awsAccessKeyId);
+    credentials.set(`${profile.profileName}.aws_secret_access_key`, profile.awsSecretAccessKey);
+    if (isConfigExist){
+        config.set(`${profile.profileName}.region`, profile.awsRegion);
+        config.set(`${profile.profileName}.output`, `json`);
+        await config.save(`${process.env.HOME}/.aws/config`);
+    };
+    await credentials.save(`${process.env.HOME}/.aws/credentials`);
+    spinner.success({ text: `New profile ${chalk.cyanBright(profile.profileName)} added succesfully.` });
+};
+
+if (args.length == 0) { chooseProfile() }
+else {
+    switch (args[0]) {
+        case 'add':
+            addProfile();
+    };
+};
